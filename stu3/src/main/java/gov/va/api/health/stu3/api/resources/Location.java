1cd9e76e8f37bb688d891928bbfd2ecf70c1190e
jhulbert <joshua.hulbert@libertyits.com>
2020-01-09 16:02:39 -0500

    Update Location stu3 to remove notnull from optional fields

--- stu3/src/main/java/gov/va/api/health/stu3/api/resources/Location.java
+++ stu3/src/main/java/gov/va/api/health/stu3/api/resources/Location.java
@@ -67,11 +67,11 @@ public class Location implements DomainResource {
 
   @Valid List<Identifier> identifier;
 
-  @NotNull Status status;
+  Status status;
 
   Coding operationalStatus;
 
-  @NotNull String name;
+  String name;
 
   List<String> alias;
 
@@ -81,15 +81,15 @@ public class Location implements DomainResource {
 
   @Valid CodeableConcept type;
 
-  @Valid @NotEmpty List<ContactPoint> telecom;
+  @Valid List<ContactPoint> telecom;
 
-  @Valid @NotNull LocationAddress address;
+  @Valid LocationAddress address;
 
   @Valid CodeableConcept physicalType;
 
   @Valid Position position;
 
-  @Valid @NotNull Reference managingOrganization;
+  @Valid Reference managingOrganization;
 
   @Valid Reference partOf;
 
