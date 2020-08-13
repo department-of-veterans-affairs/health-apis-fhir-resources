d2d25284c1225d55e862b3dd838aeb856212e84f
eclendenning-va <evan.clendenning@libertyits.com>
2020-08-13 15:42:10 -0400

    uscore r4 location model

---
+++ us-core-r4/src/test/java/gov/va/api/health/uscorer4/api/samples/SampleLocations.java
@@ -0,0 +1,42 @@
+package gov.va.api.health.uscorer4.api.samples;
+
+import gov.va.api.health.uscorer4.api.resources.Location;
+import gov.va.api.health.uscorer4.api.resources.Location.HoursOfOperation;
+import gov.va.api.health.uscorer4.api.resources.Location.Mode;
+import gov.va.api.health.uscorer4.api.resources.Location.Position;
+import gov.va.api.health.uscorer4.api.resources.Location.Status;
+import java.util.Collections;
+import java.util.List;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Delegate;
+
+@NoArgsConstructor(staticName = "get")
+public class SampleLocations {
+  @Delegate SampleDataTypes dataTypes = SampleDataTypes.get();
+
+  public Location location() {
+    return Location.builder()
+        .status(Status.active)
+        .name("My Name")
+        .alias("My Alias")
+        .hoursOfOperation(
+            HoursOfOperation.builder()
+                .allDay(true)
+                .openingTime("00:00:00")
+                .closingTime("00:00:00")
+                .daysOfWeek(List.of(Location.DaysOfWeek.mon, Location.DaysOfWeek.fri))
+                .build())
+        .description("Description")
+        .availabilityExceptions("Closed every other monday")
+        .mode(Mode.instance)
+        .type(dataTypes.codeableConcept())
+        .telecom(Collections.singletonList(dataTypes.contactPoint()))
+        .address(dataTypes.address())
+        .physicalType(dataTypes.codeableConcept())
+        .position(Position.builder().latitude(3.0).longitude(2.0).altitude(1.0).build())
+        .managingOrganization(dataTypes.reference())
+        .partOf(dataTypes.reference())
+        .endpoint(dataTypes.reference())
+        .build();
+  }
+}
