0cfa41efb492dc87627f920f6497b41f5198b54b
eclendenning-va <evan.clendenning@libertyits.com>
2020-09-25 13:13:24 -0400

    add carin-bb annotations

--- us-core-r4/src/main/java/gov/va/api/health/r4/api/resources/Patient.java
+++ us-core-r4/src/main/java/gov/va/api/health/r4/api/resources/Patient.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import gov.va.api.health.r4.api.CarinBlueButton;
 import gov.va.api.health.r4.api.Fhir;
 import gov.va.api.health.r4.api.bundle.AbstractBundle;
 import gov.va.api.health.r4.api.bundle.AbstractEntry;
@@ -85,6 +86,15 @@ public class Patient implements Resource {
   @Valid List<Extension> modifierExtension;
 
   // R4 Patient Resource
+  @CarinBlueButton(
+          note =
+                  "Slice Definition Constraints: "
+                          + "identifier.memberid && identifier:medrecnum && identifier:patacctnum"
+                          + "cardinality=1..*."
+                          + "identifier.type field cardinality=0..1"
+                          + "identifier.type.coding field cardinality=1..*"
+                          + "identifier.type.code field cardinality=1..1"
+                          + "all other slices identifier.type field cardinality=1..1")
   @Valid @NotEmpty List<Identifier> identifier;
 
   Boolean active;
