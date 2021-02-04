981139e9a19a0c72491553b10b769f9dc2c9d4e8
Adrian Garcia <adrian.garcia@libertyits.com>
2021-02-04 16:13:12 -0500

    Added mixed resource JSON for testing, and created a basic MixedResource class with a test. Currently a WIP.

---
+++ us-core-r4/src/test/java/gov/va/api/health/r4/api/resources/MixedResourceTest.java
@@ -0,0 +1,28 @@
+package gov.va.api.health.r4.api.resources;
+
+import static gov.va.api.health.autoconfig.configuration.JacksonConfig.createMapper;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+public class MixedResourceTest {
+
+  @Test
+  void mixed() {
+    String path = "/mixed-resource.json";
+    roundTrip(path, MixedResource.class);
+  }
+
+  @SneakyThrows
+  private <T> void roundTrip(String path, Class<T> clazz) {
+    T response = createMapper().readValue(getClass().getResourceAsStream(path), clazz);
+    assertThat(response).isExactlyInstanceOf(clazz);
+    String actual = createMapper().writerWithDefaultPrettyPrinter().writeValueAsString(response);
+    String expected =
+        createMapper()
+            .writerWithDefaultPrettyPrinter()
+            .writeValueAsString(createMapper().readTree(getClass().getResourceAsStream(path)));
+    assertThat(actual).isEqualTo(expected);
+  }
+}
