97e68988f49d9f0fb4f2b0c35da4dab60602a223
eclendenning-va <evan.clendenning@libertyits.com>
2021-07-12 08:55:05 -0400

    add response codes, and fake examples

--- us-core-r4/src/main/java/gov/va/api/health/r4/api/OrganizationApi.java
+++ us-core-r4/src/main/java/gov/va/api/health/r4/api/OrganizationApi.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,34 +21,61 @@ public interface OrganizationApi {
       tags = {"Organization"})
   @GET
   @Path("Organization/{id}")
-  @ApiResponse(
-      responseCode = "200",
-      description = "Record found",
-      content =
+  @ApiResponses({
+    @ApiResponse(
+        responseCode = "200",
+        description = "Record found",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = Organization.class))),
+    @ApiResponse(
+        responseCode = "400",
+        description = "Bad request",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = OperationOutcome.class))),
+    @ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = Organization.class)))
-  @ApiResponse(
-      responseCode = "400",
-      description = "Bad request",
-      content =
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "403",
+        description = "Forbidden",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = OperationOutcome.class)))
-  @ApiResponse(
-      responseCode = "404",
-      description = "Not found",
-      content =
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "404",
+        description = "Not found",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = OperationOutcome.class))),
+    @ApiResponse(
+        responseCode = "500",
+        description = "Server Error",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = OperationOutcome.class)))
+              schema = @Schema(implementation = OperationOutcome.class))
+        })
+  })
   Organization organizationRead(
       @Parameter(
               in = ParameterIn.PATH,
               name = "id",
               required = true,
               description =
-                  "The logical id of the resource. Once assigned, this value never changes.")
+                  "The logical id of the resource. Once assigned, this value never changes.",
+              example = "I3-87aih87KJHaMAuy89PKA90AOIJL897QHJKNaPZha9jLJKHAO8hkja")
           String id);
 
   @Operation(
@@ -57,40 +85,68 @@ public interface OrganizationApi {
       tags = {"Organization"})
   @GET
   @Path("Organization")
-  @ApiResponse(
-      responseCode = "200",
-      description = "Record found",
-      content =
+  @ApiResponses({
+    @ApiResponse(
+        responseCode = "200",
+        description = "Record found",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = Organization.Bundle.class))),
+    @ApiResponse(
+        responseCode = "400",
+        description = "Bad request",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = OperationOutcome.class))),
+    @ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = Organization.Bundle.class)))
-  @ApiResponse(
-      responseCode = "400",
-      description = "Bad request",
-      content =
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "403",
+        description = "Forbidden",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = OperationOutcome.class)))
-  @ApiResponse(
-      responseCode = "404",
-      description = "Not found",
-      content =
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "404",
+        description = "Not found",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = OperationOutcome.class))),
+    @ApiResponse(
+        responseCode = "500",
+        description = "Server Error",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = OperationOutcome.class)))
+              schema = @Schema(implementation = OperationOutcome.class))
+        })
+  })
   Organization.Bundle organizationSearch(
       @Parameter(
               in = ParameterIn.QUERY,
               name = "_id",
               description =
-                  "The logical id of the resource. Once assigned, this value never changes.")
+                  "The logical id of the resource. Once assigned, this value never changes.",
+              example = "I3-87aih87KJHaMAuy89PKA90AOIJL897QHJKNaPZha9jLJKHAO8hkja")
           String id,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "identifier",
               description =
                   "The logical identifier of the resource. Once assigned, this value "
-                      + "never changes.")
+                      + "never changes.",
+              example = "I3-87aih87KJHaMAuy89PKA90AOIJL897QHJKNaPZha9jLJKHAO8hkja")
           String identifier,
       @Parameter(
               in = ParameterIn.QUERY,
@@ -98,34 +154,40 @@ public interface OrganizationApi {
               description =
                   "Indicates the physical location of the organization "
                       + "expressed using postal conventions "
-                      + "(as opposed to GPS or other location definition formats).")
+                      + "(as opposed to GPS or other location definition formats).",
+              example = "2360+E+PERSHING+BLVD")
           String address,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "address-city",
-              description = "Indicates the geographical city where the organization resides.")
+              description = "Indicates the geographical city where the organization resides.",
+              example = "CHEYENNE")
           String addressCity,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "address-state",
-              description = "Indicates the geographical state where the organization resides.")
+              description = "Indicates the geographical state where the organization resides.",
+              example = "WY")
           String addressState,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "address-postalcode",
               description =
                   "Indicates the postal code that designates the region"
-                      + " where the organization resides.")
+                      + " where the organization resides.",
+              example = "82001-5356")
           String addressPostalCode,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "name",
-              description = "Indicates the name of the organization as it is referenced by humans.")
+              description = "Indicates the name of the organization as it is referenced by humans.",
+              example = "CHEYENNE+VA+MEDICAL")
           String name,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "page",
-              description = "The page number of the search result.")
+              description = "The page number of the search result.",
+              example = "2")
           @DefaultValue("1")
           int page,
       @Parameter(
@@ -133,7 +195,8 @@ public interface OrganizationApi {
               name = "_count",
               description =
                   "The number of resources that should be returned in a single page."
-                      + " The maximum count size is 100.")
+                      + " The maximum count size is 100.",
+              example = "30")
           @DefaultValue("30")
           int count);
 }
--- us-core-r4/src/main/java/gov/va/api/health/r4/api/PractitionerApi.java
+++ us-core-r4/src/main/java/gov/va/api/health/r4/api/PractitionerApi.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,27 +21,53 @@ public interface PractitionerApi {
       tags = {"Practitioner"})
   @GET
   @Path("Practitioner/{id}")
-  @ApiResponse(
-      responseCode = "200",
-      description = "Record found",
-      content =
+  @ApiResponses({
+    @ApiResponse(
+        responseCode = "200",
+        description = "Record found",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = Practitioner.class))),
+    @ApiResponse(
+        responseCode = "400",
+        description = "Bad request",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = OperationOutcome.class))),
+    @ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = Practitioner.class)))
-  @ApiResponse(
-      responseCode = "400",
-      description = "Bad request",
-      content =
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "403",
+        description = "Forbidden",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = OperationOutcome.class)))
-  @ApiResponse(
-      responseCode = "404",
-      description = "Not found",
-      content =
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "404",
+        description = "Not found",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = OperationOutcome.class))),
+    @ApiResponse(
+        responseCode = "500",
+        description = "Server Error",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = OperationOutcome.class)))
+              schema = @Schema(implementation = OperationOutcome.class))
+        })
+  })
   Practitioner practitionerRead(
       @Parameter(
               in = ParameterIn.PATH,
@@ -57,53 +84,84 @@ public interface PractitionerApi {
       tags = {"Practitioner"})
   @GET
   @Path("Practitioner")
-  @ApiResponse(
-      responseCode = "200",
-      description = "Record found",
-      content =
+  @ApiResponses({
+    @ApiResponse(
+        responseCode = "200",
+        description = "Record found",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = Practitioner.Bundle.class))),
+    @ApiResponse(
+        responseCode = "400",
+        description = "Bad request",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = OperationOutcome.class))),
+    @ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = Practitioner.class)))
-  @ApiResponse(
-      responseCode = "400",
-      description = "Bad request",
-      content =
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "403",
+        description = "Forbidden",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = OperationOutcome.class)))
-  @ApiResponse(
-      responseCode = "404",
-      description = "Not found",
-      content =
+              schema = @Schema(implementation = OperationOutcome.class))
+        }),
+    @ApiResponse(
+        responseCode = "404",
+        description = "Not found",
+        content =
+            @Content(
+                mediaType = "application/fhir+json",
+                schema = @Schema(implementation = OperationOutcome.class))),
+    @ApiResponse(
+        responseCode = "500",
+        description = "Server Error",
+        content = {
           @Content(
               mediaType = "application/fhir+json",
-              schema = @Schema(implementation = OperationOutcome.class)))
+              schema = @Schema(implementation = OperationOutcome.class))
+        })
+  })
   Practitioner.Bundle practitionerSearch(
       @Parameter(
               in = ParameterIn.QUERY,
               name = "_id",
               description =
-                  "The logical id of the resource. Once assigned, this value never changes.")
+                  "The logical id of the resource. Once assigned, this value never changes.",
+              example = "I3-87aih87KJHaMAuy89PKA90AOIJL897QHJKNaPZha9jLJKHAO8hkja")
           String id,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "identifier",
-              description = "A unique identifier for a practitioner within a given system.")
+              description = "A unique identifier for a practitioner within a given system.",
+              example = "I3-87aih87KJHaMAuy89PKA90AOIJL897QHJKNaPZha9jLJKHAO8hkja")
           String identifier,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "family",
-              description = "The family name of the practitioner.")
+              description = "The family name of the practitioner.",
+              example = "Familyname")
           String family,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "given",
-              description = "The given name of the practitioner.")
+              description = "The given name of the practitioner.",
+              example = "GivenName")
           String given,
       @Parameter(
               in = ParameterIn.QUERY,
               name = "name",
-              description = "The given or family name of the practitioner.")
+              description = "The given or family name of the practitioner.",
+              example = "Familyname")
           String name,
       @Parameter(
               in = ParameterIn.QUERY,
